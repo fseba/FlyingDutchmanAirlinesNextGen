647553e6f508e8ec96c5c0f9dfdd037cf720ff1f
sebastianfrueh <sfrueh@gmx.net>
2023-03-18 13:58:33 +0100

    Add tests for get bookings from database methods

--- FlyingDutchmanAirlines_Tests/RepositoryLayer/BookingRepositoryTests.cs
+++ FlyingDutchmanAirlines_Tests/RepositoryLayer/BookingRepositoryTests.cs
@@ -42,9 +42,9 @@ public class BookingRepositoryTests
   [DataRow(0, -1)]
   [DataRow(-1, -1)]
   [ExpectedException(typeof(ArgumentException))]
-  public async Task CreateBooking_Failure_InvalidInpute(int customerID, int flightNumber)
+  public async Task CreateBooking_Failure_InvalidInpute(int customerId, int flightNumber)
   {
-    await _repository.CreateBooking(customerID, flightNumber);
+    await _repository.CreateBooking(customerId, flightNumber);
   }
 
   [TestMethod]
@@ -52,6 +52,74 @@ public class BookingRepositoryTests
   public async Task CreateBooking_Failure_DatabaseError()
   {
     await _repository.CreateBooking(0, 1);
+  }
+
+  [TestMethod]
+  public async Task DeleteBooking_Success()
+  {
+    await _repository.CreateBooking(1, 0);
+    Booking booking = _context.Bookings.First();
+
+    await _repository.DeleteBooking(booking.BookingId);
+
+    Assert.AreEqual(0, _context.Bookings.Count());
+  }
+
+  [TestMethod]
+  [ExpectedException(typeof(ArgumentException))]
+  public async Task DeleteBooking_Failure_NegativeBookingNumber()
+  {
+    await _repository.DeleteBooking(-1);
+  }
+
+  [TestMethod]
+  [ExpectedException(typeof(BookingNotFoundException))]
+  public async Task DeleteBooking_Failure_BookingNotFound()
+  {
+    await _repository.DeleteBooking(1);
+  }
+
+  [TestMethod]
+  public async Task GetBookingById_Success()
+  {
+    await _repository.CreateBooking(1, 0);
+    Booking dbBooking = _context.Bookings.First();
+
+    Booking booking = await _repository.GetBookingById(dbBooking.BookingId);
+
+    Assert.AreSame(dbBooking, booking);
+  }
+
+  [TestMethod]
+  public async Task GetBookingsByCustomerId_Success()
+  {
+    await _repository.CreateBooking(1, 0);
+    await _repository.CreateBooking(1, 1);
+    await _repository.CreateBooking(1, 2);
+    int bookingsCount = _context.Bookings.Count();
+
+    Booking[] bookings = await _repository.GetBookingsByCustomerId(1);
+
+    Assert.AreEqual(bookingsCount, bookings.Length);
+  }
+
+  [TestMethod]
+  [ExpectedException(typeof(BookingNotFoundException))]
+  public async Task GetBookings_Failure_NoBookingsInDatabase()
+  {
+    await _repository.GetBookingsByCustomerId(1);
+  }
+
+  [TestMethod]
+  [ExpectedException(typeof(BookingNotFoundException))]
+  public async Task GetBookingsByCustomerId_Failure_CustomerNotFoundException_Wrong_CustomerId()
+  {
+    await _repository.CreateBooking(1, 0);
+    await _repository.CreateBooking(1, 1);
+    await _repository.CreateBooking(1, 2);
+    int bookingsCount = _context.Bookings.Count();
+
+    Booking[] bookings = await _repository.GetBookingsByCustomerId(2);
   }
 }
 
