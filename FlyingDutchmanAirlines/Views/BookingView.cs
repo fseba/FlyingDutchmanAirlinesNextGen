04d90ce3d042769bdef7f03cd4f369b280eeb63e
sebastianfrueh <sfrueh@gmx.net>
2023-03-20 10:28:20 +0100

    Initial commit

---
+++ FlyingDutchmanAirlines/Views/BookingView.cs
@@ -0,0 +1,18 @@
+﻿namespace FlyingDutchmanAirlines.Views;
+
+public class BookingView
+{
+  public int BookingId { get; private set; }
+  public int CustomerId { get; private set; }
+  public string CustomerName { get; private set; }
+  public FlightView FlightView { get; private set; }
+
+  public BookingView(int bookingId, int customerId, string customerName, FlightView flightView)
+  {
+    BookingId = bookingId;
+    CustomerId = customerId;
+    CustomerName = customerName;
+    FlightView = flightView;
+  }
+}
+
