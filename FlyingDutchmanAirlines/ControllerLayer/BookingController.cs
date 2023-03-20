01be130bd758186e71ee880b98c83d49ac888475
sebastianfrueh <sfrueh@gmx.net>
2023-03-20 10:27:17 +0100

    Add GetBookingsByCustomerName method

--- FlyingDutchmanAirlines/ControllerLayer/BookingController.cs
+++ FlyingDutchmanAirlines/ControllerLayer/BookingController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 
 using FlyingDutchmanAirlines.ControllerLayer.JsonData;
 using FlyingDutchmanAirlines.ServiceLayer;
+using FlyingDutchmanAirlines.Exceptions;
+using FlyingDutchmanAirlines.Views;
 
 namespace FlyingDutchmanAirlines.ControllerLayer;
 
@@ -19,12 +21,13 @@ public class BookingController : ControllerBase
 
   [HttpPost("{flightNumber}")]
   [ProducesResponseType(StatusCodes.Status201Created)]
+  [ProducesResponseType(StatusCodes.Status400BadRequest)]
   [ProducesResponseType(StatusCodes.Status500InternalServerError)]
   public async Task<IActionResult> CreateBooking([FromBody] BookingData body, int flightNumber)
   {
     if (!ModelState.IsValid)
     {
-      return StatusCode((int)HttpStatusCode.InternalServerError, ModelState.Root.Errors.First().ErrorMessage);
+      return StatusCode((int)HttpStatusCode.BadRequest, ModelState.Root.Errors.First().ErrorMessage);
     }
 
     string name = $"{body.FirstName} {body.LastName}";
@@ -33,6 +36,39 @@ public class BookingController : ControllerBase
     return (result && exception == null)
       ? StatusCode((int)HttpStatusCode.Created)
       : StatusCode((int)HttpStatusCode.InternalServerError, exception!.Message);
+  }
+
+  [HttpPost]
+  [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(BookingView))]
+  [ProducesResponseType(StatusCodes.Status404NotFound)]
+  [ProducesResponseType(StatusCodes.Status400BadRequest)]
+  public async Task<IActionResult> GetBookingsByCustomerName([FromBody] BookingData body)
+  {
+    if (!ModelState.IsValid)
+    {
+      return StatusCode((int)HttpStatusCode.BadRequest, ModelState.Root.Errors.First().ErrorMessage);
+    }
+
+    try
+    {
+      string customerName = $"{body.FirstName} {body.LastName}";
+
+      Queue<BookingView> bookings = new();
+      await foreach (BookingView booking in _bookingService.GetBookingsByCustomerName(customerName))
+      {
+        bookings.Enqueue(booking);
+      }
+
+      return StatusCode((int)HttpStatusCode.OK, bookings);
+    }
+    catch (BookingNotFoundException)
+    {
+      return StatusCode((int)HttpStatusCode.NotFound, "No bookings were found in the database");
+    }
+    catch (Exception)
+    {
+      return StatusCode((int)HttpStatusCode.BadRequest, "Bad request");
+    }
   }
 }
 
