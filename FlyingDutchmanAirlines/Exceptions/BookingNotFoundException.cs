629465c4cc0f937dccf43719e2db2ee8bdae727e
sebastianfrueh <sfrueh@gmx.net>
2023-03-22 10:21:42 +0100

    Add constructor

--- FlyingDutchmanAirlines/Exceptions/BookingNotFoundException.cs
+++ FlyingDutchmanAirlines/Exceptions/BookingNotFoundException.cs
@@ -2,9 +2,9 @@
 
 public class BookingNotFoundException : Exception
 {
-  public BookingNotFoundException()
-  {
-  }
+  public BookingNotFoundException() {  }
+
+  public BookingNotFoundException(string? message) : base(message) {  }
 
   public BookingNotFoundException(string message, Exception innerException)
     : base(message, innerException) { }
