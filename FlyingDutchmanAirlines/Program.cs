cb71f82564e5763c7ee150ee944cc3ee60a74bad
sebastianfrueh <sfrueh@gmx.net>
2023-03-22 11:14:18 +0100

    Remove commented out code

--- FlyingDutchmanAirlines/Program.cs
+++ FlyingDutchmanAirlines/Program.cs
@@ -12,12 +12,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//var connectionString = builder.Configuration.GetConnectionString("FlyingDutchmanDatabase");
 var connectionStringLocal = builder.Configuration.GetConnectionString("FlyingDutchmanDatabase_local");
 
 builder.Services
   .AddDbContext<FlyingDutchmanAirlinesContext>(options => options.UseSqlServer(connectionStringLocal))
-  //.AddTransient<FlyingDutchmanAirlinesContext, FlyingDutchmanAirlinesContext>()
   .AddTransient<FlightService, FlightService>()
   .AddTransient<BookingService, BookingService>()
   .AddTransient<FlightRepository, FlightRepository>()
