5717803c7adcef688181ce6984182e3536bd1612
sebastianfrueh <sfrueh@gmx.net>
2023-03-30 11:45:19 +0200

    Fix formatting

--- FlyingDutchmanAirlines/DatabaseLayer/Models/Airport.cs
+++ FlyingDutchmanAirlines/DatabaseLayer/Models/Airport.cs
@@ -2,13 +2,13 @@
 
 public sealed class Airport
 {
-    public int AirportId { get; set; }
+  public int AirportId { get; set; }
 
-    public string City { get; set; } = null!;
+  public string City { get; set; } = null!;
 
-    public string Iata { get; set; } = null!;
+  public string Iata { get; set; } = null!;
 
-    public ICollection<Flight> FlightDestinationNavigations { get; } = new List<Flight>();
+  public ICollection<Flight> FlightDestinationNavigations { get; } = new List<Flight>();
 
-    public ICollection<Flight> FlightOriginNavigations { get; } = new List<Flight>();
+  public ICollection<Flight> FlightOriginNavigations { get; } = new List<Flight>();
 }
--- FlyingDutchmanAirlines/DatabaseLayer/Models/Flight.cs
+++ FlyingDutchmanAirlines/DatabaseLayer/Models/Flight.cs
@@ -2,15 +2,15 @@
 
 public sealed class Flight
 {
-    public int FlightNumber { get; set; }
+  public int FlightNumber { get; set; }
 
-    public int Origin { get; set; }
+  public int Origin { get; set; }
 
-    public int Destination { get; set; }
+  public int Destination { get; set; }
 
-    public ICollection<Booking> Bookings { get; } = new List<Booking>();
+  public ICollection<Booking> Bookings { get; } = new List<Booking>();
 
-    public Airport DestinationNavigation { get; set; } = null!;
+  public Airport DestinationNavigation { get; set; } = null!;
 
-    public Airport OriginNavigation { get; set; } = null!;
+  public Airport OriginNavigation { get; set; } = null!;
 }
