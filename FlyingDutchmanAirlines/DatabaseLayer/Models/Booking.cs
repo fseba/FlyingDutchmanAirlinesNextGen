92edb68a35ac3f6f04099442026a8c2f9c6ed137
sebastianfrueh <sfrueh@gmx.net>
2023-03-30 11:46:19 +0200

    Add factory method to create Bookings objects

--- FlyingDutchmanAirlines/DatabaseLayer/Models/Booking.cs
+++ FlyingDutchmanAirlines/DatabaseLayer/Models/Booking.cs
@@ -2,13 +2,23 @@
 
 public sealed class Booking
 {
-    public int BookingId { get; set; }
+  public int BookingId { get; private set; }
 
-    public int FlightNumber { get; set; }
+  public int FlightNumber { get; private set; }
 
-    public int? CustomerId { get; set; }
+  public int? CustomerId { get; private set; }
 
-    public Customer? Customer { get; set; }
+  public Customer? Customer { get; private set; }
 
-    public Flight FlightNumberNavigation { get; set; } = null!;
+  public Flight FlightNumberNavigation { get; private set; } = null!;
+
+  public static Booking Create(int customerId, int flightNumber)
+  {
+    if (int.IsNegative(customerId) || int.IsNegative(flightNumber))
+    {
+      throw new ArgumentException("Invalid arguments provided");
+    }
+
+    return new Booking { CustomerId = customerId, FlightNumber = flightNumber };
+  }
 }
