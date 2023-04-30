53b96ed91cde8d4b5f24f7cdba0730f956764ff3
sebastianfrueh <sfrueh@gmx.net>
2023-04-30 22:19:52 +0200

    Add additional validation

--- FlyingDutchmanAirlines/DTOs/BookingDTO.cs
+++ FlyingDutchmanAirlines/DTOs/BookingDTO.cs
@@ -7,19 +7,24 @@ public class BookingDTO
   public int BookingId { get; private set; }
   public int CustomerId { get; private set; }
   public string CustomerName { get; private set; }
-  public FlightDTO FlightView { get; private set; }
+  public FlightDTO FlightDTO { get; private set; }
 
-  public BookingDTO(Booking booking, FlightDTO flightView)
+  public BookingDTO(Booking booking, FlightDTO flightDTO)
   {
-    if (booking is null || flightView is null)
+    if (booking is null || flightDTO is null)
     {
-      throw new ArgumentNullException();
+      throw new ArgumentNullException("Booking or FlightDTO is null");
+    }
+
+    if (booking.Customer is null || booking.CustomerId is null)
+    {
+      throw new ArgumentNullException("Customer information missing - Booking objects should be loaded from the database.");
     }
 
     BookingId = booking.BookingId;
-    CustomerId = booking.CustomerId!.Value;
-    CustomerName = booking.Customer!.Name;
-    FlightView = flightView;
+    CustomerId = booking.CustomerId.Value;
+    CustomerName = booking.Customer.Name;
+    FlightDTO = flightDTO;
   }
 }
 
